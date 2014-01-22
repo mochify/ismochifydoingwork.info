7a32bca6a774b3d908b3468a56b864e17c560562
Alex Kuang <alex.kuang.13@gmail.com>
2014-01-22 17:55:24 -0500

    Tweak api for GithubScore a bit so that it doesn't depend on Date.today

--- spec/lib/foreman_spec.rb
+++ spec/lib/foreman_spec.rb
@@ -1,19 +1,29 @@
 require_relative '../../lib/foreman'
 require_relative '../../lib/github_info'
 
+require 'timecop'
+
 describe Foreman do
+  before do
+    Timecop.travel(Date.new(2014, 01, 01))
+  end
+
+  after do
+    Timecop.return
+  end
+
   it "returns 0 when there has been no activity on github" do
-    GithubInfo.stub(:recent_activity_counts).with(days: Foreman.github_history_range) { [] }
+    GithubInfo.stub(:activity_counts).with(from: Date.new(2013, 11, 02), to: Date.new(2014, 01, 01)) { Array.new(0, 0) }
     Foreman.org_score.should == 0
   end
 
   it "returns 100 when there has been activity on github every day" do
-    GithubInfo.stub(:recent_activity_counts).with(days: Foreman.github_history_range) { Array.new(60, 1) }
+    GithubInfo.stub(:activity_counts).with(from: Date.new(2013, 11, 02), to: Date.new(2014, 01, 01)) { Array.new(60, 1) }
     Foreman.org_score.should == 100
   end
 
   it "returns a positive number when there is any activity on github" do
-    GithubInfo.stub(:recent_activity_counts).with(days: Foreman.github_history_range) { Array.new(30, 0) + Array.new(30, 1) }
+    GithubInfo.stub(:activity_counts).with(from: Date.new(2013, 11, 02), to: Date.new(2014, 01, 01)) { Array.new(30, 0) + Array.new(30, 1) }
     Foreman.org_score.should == 50
   end
 end
