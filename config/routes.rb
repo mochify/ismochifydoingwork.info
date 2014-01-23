b2a216f593b90db8d60b62359ff48ec162bfe8ae
Alex Kuang <alex.kuang.13@gmail.com>
2014-01-22 21:48:53 -0500

    Add basic controller for index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Ismochifydoingwork::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'report#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
