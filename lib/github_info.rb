bb73f02a590e019fe06ae42b9b2a3c2f2a38db70
Alex Kuang <alex.kuang.13@gmail.com>
2014-01-27 15:04:56 -0500

    Use class << self convention in github_info

--- lib/github_info.rb
+++ lib/github_info.rb
@@ -14,34 +14,35 @@ class GithubInfo
 
   Octokit.middleware = stack
 
-  def self.root_repositories
-    repositories = Octokit.organization_repositories(ENV.fetch("MOCHIFYDOINGWORK_ORGNAME"))
-    repositories.select { |repo| !repo.fork }
-  end
+  class << self
+    def root_repositories
+      repositories = Octokit.organization_repositories(ENV.fetch("MOCHIFYDOINGWORK_ORGNAME"))
+      repositories.select { |repo| !repo.fork }
+    end
 
+    def all_repositories
+      repositories = root_repositories
+      forks = repositories.map { |repo|
+        Octokit.forks(repo.full_name)
+      }.flatten
 
-  def self.all_repositories
-    repositories = self.root_repositories
-    forks = repositories.map { |repo|
-      Octokit.forks(repo.full_name)
-    }.flatten
+      repositories + forks
+    end
 
-    repositories + forks
-  end
+    def all_repo_commits
+      repositories = all_repositories
 
-  def self.all_repo_commits
-    repositories = self.all_repositories
+      repo_weekly_commits = repositories.map { |repo|
+        participation = Octokit.participation_stats(repo.full_name).all
+        # If the repo is empty, the participation array comes back as []
+        participation = participation.empty? ? [0] * 52 : participation
+      }
 
-    repo_weekly_commits = repositories.map { |repo|
-      participation = Octokit.participation_stats(repo.full_name).all
-      # If the repo is empty, the participation array comes back as []
-      participation = participation.empty? ? [0] * 52 : participation
-    }
+      total_weekly_commits = repo_weekly_commits.reduce { |total_commits, repo_commits|
+        total_commits.zip(repo_commits).map { |first, second| first + second }
+      }
 
-    total_weekly_commits = repo_weekly_commits.reduce { |total_commits, repo_commits|
-      total_commits.zip(repo_commits).map { |first, second| first + second }
-    }
-
-    total_weekly_commits
+      total_weekly_commits
+    end
   end
 end
