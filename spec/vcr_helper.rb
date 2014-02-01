b7986bd97f96c16394e5845acf51f187b92a81e8
Alex Kuang <alex.kuang.13@gmail.com>
2014-01-31 21:23:02 -0500

    Add defaults to ENV.fetch so it doesn't die on travis runs.

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -3,5 +3,5 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data("<GITHUB OAUTH TOKEN>") { ENV.fetch("MOCHIFYDOINGWORK_OAUTH") }
+  config.filter_sensitive_data("<GITHUB OAUTH TOKEN>") { ENV.fetch("MOCHIFYDOINGWORK_OAUTH", "OAUTH_NOT_FOUND") }
 end
