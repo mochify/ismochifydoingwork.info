f3e128318af69a090672e9ca8a0fbc43c8272a56
Alex Kuang <alex.kuang.13@gmail.com>
2014-01-31 21:35:23 -0500

    Add orgname to sensitive data list so vcr works on travis

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -4,4 +4,5 @@ VCR.configure do |config|
   config.cassette_library_dir = "fixtures/cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data("<GITHUB OAUTH TOKEN>") { ENV.fetch("MOCHIFYDOINGWORK_OAUTH", "OAUTH_NOT_FOUND") }
+  config.filter_sensitive_data("<GITHUB ORG>") { ENV.fetch("MOCHIFYDOINGWORK_ORGNAME", "OAUTH_NOT_FOUND") }
 end
